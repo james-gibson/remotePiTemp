8348396844a55aabc17a2e04bc97ea42e5119ff7
Teagan Glenn <teagan@Teagans-Air.hsd1.co.comcast.net>
2016-02-15 18:03:44 -0700

    Method to set up sensors and store them in global variable

--- src/main.js
+++ src/main.js
@@ -7,11 +7,37 @@ function recordTemperature(){
     ds18b20.sensors(function(err, ids) {
         for(index in ids){
             ds18b20.temperature(ids[index], function(err, value) {
+var sensors = [];
                 logger.log(value);
             });
         }
     });
 }
+function setupSensors() {
+    var promise = new Promise(function (resolve, reject) {
+        var tmpSensors;
+        var response;
+
+        tmpSensors = [];
+        ds18b20.sensors(function(err, ids) {
+            for(index in ids){
+                tmpSensors.push(ids[index]);
+                console.log(ids[index]);
+            }
+            console.log('Are all sensors accounted for? [y/n] ');
+            prompt.start();
+            prompt.get(['inputResponse'], function (err, result) {
+                if (result.inputResponse.toLowerCase() == 'y') {
+                    resolve(tmpSensors);
+                } else {
+                    reject('Please reconnect sensors and retry.');
+                }
+            });
+        });
+    });
+
+    return promise;
+};
 
 var jobId = crontab.scheduleJob(config.schedule, recordTemperature);
 console.log('CronJob scheduled: '+ jobId);
